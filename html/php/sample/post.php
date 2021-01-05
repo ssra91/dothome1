b58123f375bc8e7d274088d9f2e538c3f8c83f0a
ssra91 <sora6418@gmail.com>
2021-01-05 22:51:14 +0900

    2021.01.05

---
+++ html/php/sample/post.php
@@ -0,0 +1,9 @@
+<?php
+echo $_POST['userEmail'], "<br>";
+echo $_POST['userName'], "<br>";
+echo $_POST['userNickName'], "<br>";
+echo $_POST['userPW'], "<br>";
+echo $_POST['birthYear'], "<br>";
+echo $_POST['birthMonth'], "<br>";
+echo $_POST['birthDay'], "<br>";  
+?>
---
+++ php/sample/get.php
@@ -0,0 +1,6 @@
+<?php
+//http://ssra11.dothome.co.kr/php/sample/get.php?type=php&book=react
+
+echo $_GET['type'],"<br>";
+echo $_GET['book'];
+?>
